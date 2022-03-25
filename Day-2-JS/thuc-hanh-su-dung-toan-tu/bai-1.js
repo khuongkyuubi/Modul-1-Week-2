54e4c9c3e53921c718707d71f008fb618294f7bf
My.Project <khuong.kyuubi@gmail.com>
2022-03-25 15:27:30 +0700

    thêm bài thực hành sử dụng toán tử

---
+++ Day-2-JS/thuc-hanh-su-dung-toan-tu/bai-1.js
@@ -0,0 +1,6 @@
+let inpupWidth = prompt("Enter the width: ");
+let inputHeight = prompt("Enter the height: ");
+let width = parseInt(inpupWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("The area is : " + area);
