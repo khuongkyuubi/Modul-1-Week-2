c837e439bd915d906605ab89eb3b53fcff3d999e
My.Project <khuong.kyuubi@gmail.com>
2022-03-26 16:49:44 +0700

    thêm bài tập ứng dụng vui Do You Love Me

--- Day-4-JS/bai-tap-may-tinh-don-gian/js/main.js
+++ Day-4-JS/bai-tap-may-tinh-don-gian/js/main.js
@@ -1,5 +1,6 @@
 let arr = [];
 let buttonList = document.getElementsByTagName("button"); // trả về 1 HTML collection (giống array)
+let result = 0;
 for (let i = 0; i < buttonList.length; i++) {
     buttonList[i].onclick = function () {
         arr[i] = this.innerText;
@@ -7,13 +8,28 @@ for (let i = 0; i < buttonList.length; i++) {
             case "C":
                 document.getElementById("display").innerText = "";
                 break;
+            case "+":
+                add();
+                // document.getElementById("display").innerText += "";
+                break;
             case "=":
-                document.getElementById("display").innerText += "";
+                document.getElementById("display").innerText = result;
                 break;
             default:
                 document.getElementById("display").innerText += this.innerText; //
         }
     };
-    console.log(arr);
+    function add() {
+        let num = 1*document.getElementById("display").innerText;
+        document.getElementById("display").innerText = "";
+        // let result = 0;
+        result += num;
+        // document.getElementById("display").innerText += "";
+        return result;
+    }
+
+
+
+    console.log(result);
 }
 console.log(arr);
